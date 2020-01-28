4685c4657a99dd38ec72f4d823b2e849cc833fc2
alee <alee@fwparker.org>
2020-01-28 13:25:21 -0600

    Added imports notes

---
+++ Notes/NotesB/01_imports.py
@@ -0,0 +1,28 @@
+import math
+import random as r
+from math import *
+from import_me import settings
+from import_me.settings import *
+import pandas as pd
+
+
+if __name__ == "__main__":
+    # this code only runs if you opened THIS FILE
+    # import library directly
+    print(math.pi)
+
+    # import and assign a shortcut name
+    print(r.randrange(1, 7))
+
+    # import everything using wildcard *
+    print(pi)
+
+    # importing your own python files (modules)
+    print(settings.a)
+
+    # import with wildcard
+    print(a)
+
+    print(double(double(a)))
+
+
---
+++ Notes/NotesB/import_me/settings.py
@@ -0,0 +1,9 @@
+a = 5
+
+def double(x):
+    x *= 2
+    return x
+
+
+if __name__ == "__main__":
+    print("a =", a)
