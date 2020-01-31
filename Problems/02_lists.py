51451aecea0b789c1283f8c19f40b0e5d7915b20
alee <alee@fwparker.org>
2020-01-31 08:06:24 -0600

    Added imports notes

--- Problems/02_lists.py
+++ Problems/02_lists.py
@@ -1,12 +1,13 @@
-# LISTS (21pts)
+# LISTS (25pts)
 # Show work on all problems.  Manually finding the answer doesn't count
 
-# PROBLEM 1 (Using List Comprehensions - 6pts)
+# PROBLEM 1 (Using List Comprehensions - 8pts)
 # Use list comprehensions to do the following:
 # a) Make a list of numbers from 1 to 100
 # b) Make a list of even numbers from 20 to 40
 # c) Make a list of squares from 1 to 100 (1 ** 2 to 100 ** 2)
-
+# d) Make a list of all positive numbers in my_list below.
+my_list = [-77, -78, 82, 81, -40, 2, 62, 65, 74, 48, -37, -52, 90, -84, -79, -45, 47, 60, 35, -18]
 
 # PROBLEM 2 (Import the number list - 3pts)
 # The Problems directory contains a file called "number_list.py"
@@ -25,8 +26,15 @@
 # PROBLEM 4 (4pts)
 # Find the number which appears most often in num_list?
 
-# CHALLENGE
+
+# CHALLENGE PROBLEMS (2pts)
+# TOUGH PROBLEMS, BUT FEW POINTS
+
 # Find the number of prime numbers in num_list?
+# Hint: One way is to just start removing the ones that aren't
+
+# Find the number of palindromes
+# Hint: This may be easier to do with strings
 
 
 
