58f5b232ccf63b121c2a958ceb34aa6a58a7015a
alee <alee@fwparker.org>
2020-01-31 10:37:14 -0600

    Added imports notes

--- Notes/NotesA/02_lists_and_comprehensions.py
+++ Notes/NotesA/02_lists_and_comprehensions.py
@@ -82,3 +82,96 @@ print(my_list + my_numlist)
 
 
 #  ITERATING THROUGH A LIST
+# FOR EACH LOOP  (works with a copy of the item)
+for name in my_list:
+    name = name + "!"
+    print(name)
+
+print(my_list)
+
+# INDEX VARIABLE LOOP (works directly with the list using indexes)
+for i in range(len(my_list)):
+    my_list[i] = my_list[i].lower()
+    print(my_list[i])
+
+print(my_list)
+
+# LIST COMPREHENSIONS
+# [returned_item FOR iterator IN list/range IF filter]
+
+# make a list from 50 to 100
+my_list = []
+
+for i in range(50, 101):
+    my_list.append(i)
+
+print(my_list)
+
+
+my_list = [x for x in range(50, 101)]  # does same as above
+print(my_list)
+
+
+# make a list of powers of 2 from 0 to 50
+my_list = []
+
+for i in range(51):
+    my_list.append(2 ** i)
+
+print(my_list)
+
+
+my_list = [2 ** x for x in range(51)]
+
+
+
+# same list, but filter out any results greater than 100000
+my_list = []
+
+for i in range(51):
+    if 2 ** i < 100000:
+        my_list.append(2 ** i)
+
+print(my_list)
+
+
+my_list = [2 ** x for x in range(51) if 2 ** x < 100000]
+print(my_list)
+
+
+#  roll a single die 100 times and add it to a list
+roll = random.randrange(1, 7)  # remember me?
+
+my_list = [random.randrange(1, 7) for x in range(100)]
+print(my_list)
+
+# roll two die 100 times and add it to a list (ex: [1, 6])
+my_list = [[random.randrange(1, 7), random.randrange(1, 7)] for x in range(100)]
+print(my_list)
+
+
+# filter out so we only show doubles ([1, 1] etc.)
+my_doubles = [x for x in my_list if x[0] == x[1]]
+print(my_doubles)
+
+
+# all at once: create a list of two die rolls, filter out the doubles
+my_doubles = [x for x in [[random.randrange(1, 7), random.randrange(1, 7)] for x in range(100)] if x[0] == x[1]]  # generates two die rolls and filters out for doubles
+print(my_doubles)
+
+
+
+# Working with strings is similar to lists
+first = "Francis"
+last = "Parker"
+print(first[0])
+first = first.upper()
+print(first)
+print(first + last)
+print(last[-3:])
+
+if "Par" in last:
+    print("YEP")
+
+for letter in first:
+    print(letter)
