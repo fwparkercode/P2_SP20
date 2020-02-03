f1c73b02c60c39c05daebe6d333f7ab23cef9f2f
alee <alee@fwparker.org>
2020-02-03 08:19:14 -0600

    Added imports notes

---
+++ Labs/hangman.py
@@ -0,0 +1,86 @@
+# Hangman game
+
+# PSEUDOCODE
+# setup your game by doing the following
+# make a word list for your game
+# grab a random word from your list and store it as a variable
+
+# in a loop, do the following
+# display the hangman using the gallows
+# display the used letters so the user knows what has been selected
+# display the length of the word to the user using blank spaces and used letters
+# prompt the user to guess a letter
+# don't allow the user to select the same letter twice
+# if the guess is incorrect increment incorrect_guesses by 1
+# if the incorrect_guesses is greater than 8, tell the user they lost and exit the program
+# if the user gets all the correct letters, tell the user they won
+
+# ask if they want to play again
+
+gallows = [
+    '''
+      +---+
+      |   |
+          |
+          |
+          |
+          |
+    =========
+    ''',
+    '''
+      +---+
+      |   |
+      O   |
+          |
+          |
+          |
+    =========
+    ''',
+    '''
+      +---+
+      |   |
+      O   |
+      |   |
+          |
+          |
+    =========
+    ''',
+    '''
+      +---+
+      |   |
+      O   |
+     /|   |
+          |
+          |
+    =========
+    ''',
+    '''
+      +---+
+      |   |
+      O   |
+     /|\  |
+          |
+          |
+    =========
+    ''',
+    '''
+      +---+
+      |   |
+      O   |
+     /|\  |
+     /    |
+          |
+    =========
+    ''',
+    '''
+      +---+
+      |   |
+      O   |
+     /|\  |
+     / \  |
+          |
+    =========
+    '''
+    ]
+
+print(gallows[4])
