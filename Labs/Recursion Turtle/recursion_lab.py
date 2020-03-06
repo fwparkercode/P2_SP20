a4b4c934df98fdf918b1d1bf7ee9136c28eb491f
alee <alee@fwparker.org>
2020-03-06 05:56:23 -0600

    Added imports notes

---
+++ Labs/Recursion Turtle/recursion_lab.py
@@ -0,0 +1,18 @@
+'''
+Turtle Recursion (30pts)
+
+1)  Using the turtle library, create the H fractal pattern as shown in the image (htree4.jpg) in this folder. (15pts)
+
+2)  Using the turtle library, create any of the other recursive patterns in the data folder. (10pts)
+
+3)  Create your own work of art with a repeating pattern of your making.  It must be a repeated pattern using recursion. Snowflakes, trees, and spirals are a common choice.  (5pt)
+
+
+ Each fractal should
+ - be recursive
+ - have a depth of at least 4
+ - be contained on the screen
+
+  Have fun!
+
+'''
