a7adf99ec5c28912095e6e107d70c37b602ce3f5
alee <alee@fwparker.org>
2020-03-12 12:54:43 -0500

    Added imports notes

---
+++ Notes/NotesA/10_plotting.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import random
+
+plt.figure(1)  # create a new window/plot
+
+plt.plot([1, 2, 3, 4])  # plot y against the index
+
+plt.show()  # opens the window/plot
