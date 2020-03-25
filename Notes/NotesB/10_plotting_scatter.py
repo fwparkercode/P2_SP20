27c5defa2c33fa1c01a590c003b744026dfa67cf
alee <alee@fwparker.org>
2020-03-25 13:16:40 -0500

    Added imports notes

--- Notes/NotesB/10_plotting_scatter.py
+++ Notes/NotesB/10_plotting_scatter.py
@@ -3,4 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 with open("World firearms murders and ownership - Sheet 1.tsv") as f:
-    reader
+    reader = csv.reader(f, delimiter='\t')
+    data = list(reader)
+
+print(data)
