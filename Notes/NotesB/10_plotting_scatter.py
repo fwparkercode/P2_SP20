0a3e545ca253a8c73e536cd1dcf49e55e2e85daf
alee <alee@fwparker.org>
2020-03-25 13:27:40 -0500

    Added imports notes

--- Notes/NotesB/10_plotting_scatter.py
+++ Notes/NotesB/10_plotting_scatter.py
@@ -6,4 +6,24 @@ with open("World firearms murders and ownership - Sheet 1.tsv") as f:
     reader = csv.reader(f, delimiter='\t')
     data = list(reader)
 
-print(data)
+header = data.pop(0)
+print(header)
+
+# make a scatter of firearms_per_100 vs homicides_100k
+
+homicide_100k = []
+firearms_100 = []
+countries = []
+
+for country in data:
+    try:
+        homicides = float(country[5])
+        firearms = float(country[-2])
+        name = country[0]
+        homicide_100k.append(homicides)
+        firearms_100.append(firearms)
+        countries.append(name)
+    except:
+        print(country[0], "data is inadequate.")
+
+print(countries)
