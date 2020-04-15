616de548f76cdf8ab8f04dd0df6a1296424439d8
alee <alee@fwparker.org>
2020-04-15 12:31:54 -0500

    Added imports notes

---
+++ Labs/Scraping Lab/scraping-lab.py
@@ -0,0 +1,24 @@
+# SCRAPING PROBLEMS (15pts)
+# Go to your favorite follow on Twitter.  (not someone who posts explicit materials please)
+# Inspect the twitter feed in Chrome.
+# You'll notice that the tweets are stored in a ordered list <ol></ol>, and individual tweets are contained as list items <li></li>.
+# Use BeautifulSoup and requests to grab the text contents of last 5 tweetslocated on the twitter page you chose.
+# Print the tweets in a nicely formatted way.
+# Have fun.  Again, nothing explicit.
+
+
+# Weather Scraping (15pts)
+# Below is a link to a 10-day weather forecast at weather.com
+# Pick the weather for a city that has the first letter as your name.
+# Use requests and BeautifulSoup to scrape data from the weather table.
+# Print a synopsis of the weather for the next 10 days.
+# Include the day and date, description, high and low temp, chance of rain, and wind. (2pts each)
+# Print the weather for each of the next 10 days to the user in a readable sentences.
+# You can customize the text as you like, but it should be readable as a sentence without errors. (5pts)
+# You will need to target specific classes or other attributes to pull some parts of the data.
+# Sample sentence:
+# Wednesday, April 4 will be Partly Cloudy/Windy with a High of 37 degrees and a low of 25, humidity at 52%.  There is 0% chance of rain with winds out of the WNW at 22 mph.
+# if the sentence is a little different than shown, that will work; do what you can.  Don't forget about our friend string.format()
+
+
+
