99f4a13e79c4e4efa2f5af371e9edda4a00733fb
alee <alee@fwparker.org>
2020-04-21 06:38:00 -0500

    Added imports notes

---
+++ Labs/CTA Map/L_stations.py
@@ -0,0 +1,15 @@
+# Folium train map
+
+# 15pts - Use folium to plot all of the L train stops in Chicago. Use an appropriate start zoom level.
+# 5pts - Add the name to each stop as a popup. Add a train icon to each marker.  Save as an html page in the same folder.
+# 3pts  - Color code all of the lines (make the pink line marker pink etc.)
+# 2pts - Brown is not a default color name.  See if you can use the documentation for Folium to set a marker color through other means.
+
+# Data set is in this folder, but can be found at: https://data.cityofchicago.org/api/views/8pix-ypme/rows.csv?accessType=DOWNLOAD
+
+# Note: Location is in tuple form, but is of course a string.
+
+import folium
+import csv
+
+
