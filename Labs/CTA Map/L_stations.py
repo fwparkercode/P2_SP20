eb11aa85ed0149158ad99340326a2cef6b9a2d7c
alee <alee@fwparker.org>
2020-04-21 06:54:38 -0500

    Added imports notes

--- Labs/CTA Map/L_stations.py
+++ Labs/CTA Map/L_stations.py
@@ -1,5 +1,6 @@
 # Folium train map
 
+
 # 15pts - Use folium to plot all of the L train stops in Chicago. Use an appropriate start zoom level.
 # 5pts - Add the name to each stop as a popup. Add a train icon to each marker.  Save as an html page in the same folder.
 # 3pts  - Color code all of the lines (make the pink line marker pink etc.)
@@ -7,9 +8,13 @@
 
 # Data set is in this folder, but can be found at: https://data.cityofchicago.org/api/views/8pix-ypme/rows.csv?accessType=DOWNLOAD
 
-# Note: Location is in tuple form, but is of course a string.
+# Tricky parts of this one
+## The location is in tuple format.  If you have trouble converting it, try this:
+my_string = '(41.2, -87.9)'
+my_tuple = eval(my_string)
+print(my_tuple)
+print(type(my_tuple))
 
-import folium
-import csv
 
+# If you have extra time, try to put some html into the popup.
 
