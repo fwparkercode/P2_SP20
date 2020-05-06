0edd77ca87d56fdedf85ec0cd34b132dcff35e9c
alee <alee@fwparker.org>
2020-05-06 13:07:46 -0500

    Added imports notes

--- Notes/NotesA/14_pandas.py
+++ Notes/NotesA/14_pandas.py
@@ -57,7 +57,41 @@ iso_and_rank = df3.loc[:, ['ISO code', 'Rank by rate of ownership']]
 print(type(iso_and_rank))
 
 
+# WORLD CUP
 
-# World cup matches
+import pandas as pd
+x = 5
+x  # in ipython/console, this automatically prints
+
+df = pd.read_csv('/Users/alee/PycharmProjects/P2_SP20/Notes/NotesB/world_cup_matches.csv')  # use full path when working in console
+
+
+# iloc (only useful for index number)
+df.iloc[3:6]  # look at 3, 4, 5 matches
+df.iloc[3:6, [4, 7]]   # cols 4 and 7 for index/rows 3to5
+
+# loc
+df.loc[3:6, ['Home Team Name', 'Away Team Name']] # use col names to slice
+
+# all games from 1950
+df_1950 = df.loc[df['Year'] == 1950]
+
+# all games from 1950 Group 3
+df_1950.loc[df['Stage'] == 'Group 3']
+
+# alternately we could do both filters at once
+df.loc[(df['Year'] == 1950) & (df['Stage'] == 'Group 3')]  # loc[(cond1) & (cond2)]
+
+
+# Number of home games played by Netherlands
+df.loc[df['Home Team Name'] == 'Netherlands'].count()
+
+# All games by Netherlands
+df.loc[(df['Home Team Name'] == "Netherlands") | (df['Away Team Name'] == "Netherlands")]
+
+
+
+
+
 
 
